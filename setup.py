c0d76b2f84ab7d1a3b596a0314bc9d4797a0b649
thiele67 <>
2021-12-10 14:51:16 +0100

    Updated setup.py for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
+import setuptools
 from setuptools import setup
 
 setup(
     name='gfit',
     version='0.1',
-    packages=['gfit'],
-    url='',
-    license='',
+    url='https://github.com/samthiele/gfit',
+    license='MIT',
     author='Sam Thiele',
-    author_email='',
+    author_email='sam.thiele01@gmail.com',
     description='Fast multi-gaussian curve fitting',
-    install_requires = ['numpy','scipy','numba','tqdm'],
+    long_description='Convert spectra or other signals to the sum of multiple symmetric or asymmetric gaussian functions.',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Topic :: Scientific/Engineering"
+        ],
+    keywords='gaussian data fitting spectral features',
+    python_requires='>=3.6',
+    install_requires=['numpy', 'scipy', 'numba', 'tqdm'],
+    project_urls={  # Optional
+            'Source': 'https://github.com/samthiele/gfit',
+        },
 )
